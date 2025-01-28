cdedca5801290f8fe0426fccfe3e9b43e844c4d3
ArthasGD <mr.avatar2@mail.ru>
2025-01-29 00:31:56 +0500

    Инициализация репозитория

---
+++ core_tests/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru or en")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    
+    print("\n start browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\n quit browser..")
+    browser.quit()
