9a41fe932fd8c7ced654f1a80a826b871696c6ec
ArthasGD <mr.avatar2@mail.ru>
2025-05-10 16:43:45 +0500

    - init py - conftest.py теперь не берет версию версию хром браузера с пк, а создает сам при выполнении модуля

--- core_tests/tests/conftest.py
+++ core_tests/tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
@@ -8,12 +10,13 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
+    service = Service(executable_path=ChromeDriverManager().install())
     language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
     
     print("\n start browser for test..")
-    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome(service=service, options=options)
     yield browser
     print("\n quit browser..")
     browser.quit()
