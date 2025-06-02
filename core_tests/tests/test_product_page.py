d9abd833f7a82576aff347c8e3c02f6d8ca33cf3
Miririleones <mr.avatar2@mail.ru>
2025-06-03 00:10:19 +0500

    удалил отладку

--- core_tests/tests/test_product_page.py
+++ core_tests/tests/test_product_page.py
@@ -2,7 +2,7 @@ from pages.product_page import ProductPage
 import time
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_page()
@@ -10,6 +10,5 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_to_basket_button()
     page.click_add_to_basket_button()
     page.solve_quiz_and_get_code()
-    # time.sleep(120)
     page.should_be_product_name()
     page.should_be_product_cost()
