f42a1abe6205a232a4705fc41ac7718d902d4ac1
Miririleones <mr.avatar2@mail.ru>
2025-07-14 21:53:22 +0500

    Закоментил детали запуска в readme, закоментил часть conftest

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pytest-pageobject
 пет проект для написания тестов
+
+core_tests/tests pytest -s -v test_product_page.py 
--- core_tests/tests/conftest.py
+++ core_tests/tests/conftest.py
@@ -4,16 +4,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose language: ru or en")
+# def pytest_addoption(parser): -- ПОДУМАТЬ КАК РАБОТАЕТ
+#     parser.addoption('--language', action='store', default='en',
+#                      help="Choose language: ru or en")
 
 @pytest.fixture(scope="function")
-def browser(request):
+def browser():
     service = Service(executable_path=ChromeDriverManager().install())
-    language = request.config.getoption("language")
+    # language = request.config.getoption("language") -- ПОДУМАТЬ КАК РАБОТАЕТ
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    # options.add_experimental_option('prefs', {'intl.accept_languages': language}) -- ПОДУМАТЬ КАК РАБОТАЕТ
     options.add_argument('--headless')
     
     print("\n start browser for test..")
